18a5fcfea08d7da3ae70ec666ec934297bdef183
jithinsp <jithinsp001@gmail.com>
2024-03-13 12:18:24 +0530

    SSL

--- src/app/core/constants/baseurls.constant.ts
+++ src/app/core/constants/baseurls.constant.ts
@@ -1,8 +1,16 @@
 export const BASE_URL = ["http://localhost:8060/"]
 export const USER_SERVICE = ["http://localhost:8084/"]
 
-export const API_AUTH_SERVICE = ["http://13.235.234.225:8083/"]
-export const API_USER_SERVICE = ["http://13.235.234.225:8084/"]
-export const API_ACADEMICS_SERVICE = ["http://35.154.161.169:8085/"]
-export const API_FINANCE_SERVICE = ["http://35.154.161.169:8086/"]
-export const API_NOTIFICATION_SERVICE = ["http://3.110.4.78:8087/"]
+export const SSL_URL = ["api.techzonline.shop"]
+
+export const API_AUTH_SERVICE = SSL_URL
+export const API_USER_SERVICE = SSL_URL
+export const API_ACADEMICS_SERVICE = SSL_URL
+export const API_FINANCE_SERVICE = SSL_URL
+export const API_NOTIFICATION_SERVICE = SSL_URL
+
+// export const API_AUTH_SERVICE = ["http://13.235.234.225:8083/"]
+// export const API_USER_SERVICE = ["http://13.235.234.225:8084/"]
+// export const API_ACADEMICS_SERVICE = ["http://35.154.161.169:8085/"]
+// export const API_FINANCE_SERVICE = ["http://35.154.161.169:8086/"]
+// export const API_NOTIFICATION_SERVICE = ["http://3.110.4.78:8087/"]
